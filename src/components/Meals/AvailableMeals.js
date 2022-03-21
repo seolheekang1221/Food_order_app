588e187e335de7b1cf45453426016943e2ca3ff4
seolhee <seolheekang1221@gmail.com>
2022-03-21 15:55:55 -0400

    handling errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,11 +7,19 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       setIsLoading(true);
-      const response = await fetch('https://foodorderapp-57c11-default-rtdb.firebaseio.com/');
+      const response = await fetch(
+        'https://foodorderapp-57c11-default-rtdb.firebaseio.com/'
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -28,14 +36,25 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-  
-    fetchMeals();
-  }, []);
+
+        fetchMeals().catch((error) => {
+          setIsLoading(false);
+          setHttpError(error.message);
+        });
+    }, []);
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  };
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
