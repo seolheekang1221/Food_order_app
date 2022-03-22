e86b672675329cd297511ce504f51bccaffc7920
seolhee <seolheekang1221@gmail.com>
2022-03-22 09:48:55 -0400

    git adding a checkout

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,32 @@
+import classes from "./Checkout.module.css";
+
+const Checkout = (props) => {
+  const confirmHandler = (event) => {
+    event.preventDefault();
+  };
+  
+  return (
+    <form onSubmit={confirmHandler}>
+      <div className={classes.control}>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name"></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="name">Street</label>
+        <input type="text" id="street"></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="postal">Postal code</label>
+        <input type="text" id="postal"></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="postal">City</label>
+        <input type="text" id="city"></input>
+      </div>
+      <button type="button" onClick={props.onCancel}>Cancel</button>
+      <button>Confirm</button>
+    </form>
+  );
+};
+
+export default Checkout;
